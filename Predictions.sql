02314f3d734e9e7e39b828b1df468f4d92c2ce53
Pavel Koryakin <pkoryakin@devteam.powerhub.com>
2020-05-18 15:35:41 -0400

    -added Predictions table

---
+++ Predictions.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Predictions]
+(
+	[Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY DEFAULT NEWID(), 
+    [ThrowId] UNIQUEIDENTIFIER NOT NULL, 
+    [Prediction] TINYINT NOT NULL, 
+    [Confidence] FLOAT NOT NULL, 
+    CONSTRAINT [FK_Predictions_ToTableThrows] FOREIGN KEY ([ThrowId]) REFERENCES [Throws]([Id])
+)
