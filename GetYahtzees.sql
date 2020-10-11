32f8140632c908b2bf6e50dcd2ad74e695af771f
Pavel Koryakin <pkoryakin@devteam.powerhub.com>
2020-10-11 03:03:47 -0400

    -added Throws.IsYahtzee column

---
+++ GetYahtzees.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[GetYahtzees]
+AS
+BEGIN
+	SELECT p.ThrowId, p.Prediction, t.DateCreated
+	FROM Throws t WITH(NOLOCK)
+	JOIN Predictions p WITH(NOLOCK) ON t.Id = p.ThrowId
+	WHERE t.IsYahtzee = 1
+END
